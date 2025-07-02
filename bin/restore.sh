23b122a7b14083e8d5b4ee2a33f1440226d5b4bc
SarthakMirani <miranisarthak.dev@gmail.com>
2025-07-02 22:20:45 +0530

    completed restore script

--- bin/restore.sh
+++ bin/restore.sh
@@ -11,3 +11,24 @@ TARGET_DIR="$HOME"
 echo "Source: $SOURCE_DIR"
 echo "Target: $TARGET_DIR"
 
+dotfiles=(
+  ".bash_logout"
+  ".config"
+  ".gtkrc-2.0"
+  ".icons"
+  ".local"
+  ".profile"
+  ".themes"
+)
+
+for file in "${dotfiles[@]}"; do
+  SRC="$SOURCE_DIR/$file"
+  DEST="$TARGET_DIR/$file"
+
+  if [ -e "$SRC" ]; then
+    rsync -a --relative "$SRC" "$TARGET_DIR"
+    echo "🔄 Restored: $file"
+  else
+    echo "⚠️  Backup for $file not found — skipping."
+  fi
+done
